ef7e468931c83fa63086758bd190ac97ac36d814
shin-tran <ngoc.shintrann@gmail.com>
2026-02-12 05:42:19 +0700

    feat(auth): implement JWT callbacks for user session management and enhance proxy authentication logic

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,39 @@
 import NextAuth from "next-auth";
-import type { NextRequest } from "next/server";
-
 import authConfig from "./auth.config";
 
 const { auth } = NextAuth(authConfig);
 
-export async function proxy(request: NextRequest) {
-  return auth(request as any, {} as any);
-}
+const authRoutes = ["/login"];
+const protectedRoutes = ["/user", "/settings"];
+
+export const proxy = auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+  const path = nextUrl.pathname;
+
+  // login rồi và vẫn vào auth route thì redirect home page
+  if (isLoggedIn && authRoutes.includes(path))
+    return Response.redirect(new URL("/", nextUrl));
+
+  const isProtected = protectedRoutes.some((route) => path.startsWith(route));
+
+  // chưa login mà vào protected route thì bắt login
+  if (isProtected && !isLoggedIn) {
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) callbackUrl += nextUrl.search;
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+    return Response.redirect(
+      new URL(`/login?callback=${encodedCallbackUrl}`, nextUrl),
+    );
+  }
+
+  // để thằng bé đó yên
+  return;
+});
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|icon|avatars|sitemap.xml|.well-known|robots.txt|manifest.webmanifest|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
 };
