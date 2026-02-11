9174949b966a67de6eb6b459cdf561fa41099228
shin-tran <ngoc.shintrann@gmail.com>
2026-02-11 11:07:14 +0700

    fix(eslint-config): refactor ESLint configuration for improved readability and structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,52 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslintConfigPrettier from "eslint-config-prettier";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const eslintConfig = defineConfig([
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked, // Check cả type
+  tseslint.configs.stylisticTypeChecked, // Check type để quyết định phong cách
+  nextVitals,
+  nextTs,
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "dist/**",
+    "build/**",
+    "next-env.d.ts",
+    "node_modules/**",
+  ]),
 
-const eslintConfig = [{
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}, ...compat.extends("next/core-web-vitals", "next/typescript"), {
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off", // Disable 'no-explicit-any'
-    "@typescript-eslint/no-unused-vars": "off", // Disable 'no-unused-vars'
+  // Đặt ở cuối để tự động tắt các rule xung đột và bật rule prettier
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.mjs"],
+          defaultProject: "tsconfig.json",
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      ...eslintConfigPrettier.rules,
+
+      "@typescript-eslint/no-explicit-any": "off", // Disable 'no-explicit-any'
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+
+      "prettier/prettier": "warn",
+    },
   },
-}];
+]);
 
 export default eslintConfig;
