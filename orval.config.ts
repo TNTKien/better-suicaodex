2886ec8c40b2b338c8d8f3e84ffedbe813276896
iamneyk <iamneyk@gmail.com>
2026-02-24 18:38:12 +0700

    feat: add initial orval configuration for weebdex API

---
+++ orval.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  weebdex: {
+    output: {
+      baseUrl: 'https://wd.memaydex.com',
+      mode: 'tags-split',
+      target: './src/lib/weebdex/hooks',
+      schemas: './src/lib/weebdex/model',
+      client: 'react-query',
+    //   mock: true,
+    },
+    input: {
+      target: './src/lib/weebdex/docs/weebdex-api-docs-v3.json'
+    },
+  },
+});
