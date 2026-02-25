17c400b834e973caba4d024a7efa4daf5bc235b4
iamneyk <iamneyk@gmail.com>
2026-02-25 18:45:41 +0700

    feat: implement parseMangaTitle and generateJsonLd functions for manga data processing

---
+++ src/lib/weebdex/utils.ts
@@ -0,0 +1,48 @@
+import { siteConfig } from "@/config/site";
+import { Manga } from "./model";
+import { generateSlug } from "../utils";
+
+/**
+ * parse manga title from manga data
+ * - title: vi > original title
+ * - altTitles: flattened from alt_titles
+ */
+export const parseMangaTitle = (
+  manga: Manga,
+): {
+  title: string;
+  altTitles: string[];
+} => {
+  const viTitle = manga.alt_titles?.["vi"]?.[0];
+  const title = viTitle ?? manga.title ?? "";
+
+  const altTitlesRaw = Object.entries(manga.alt_titles ?? {})
+    .filter(([lang]) => lang !== "vi")
+    .flatMap(([, values]) => values);
+
+  const altTitles =
+    viTitle && manga.title ? [manga.title, ...altTitlesRaw] : altTitlesRaw;
+
+  return { title, altTitles };
+};
+
+export const generateJsonLd = (manga: Manga) => {
+  const { title } = parseMangaTitle(manga);
+  const description =
+    manga.description || `Đọc truyện ${manga.title} - SuicaoDex`;
+  return {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    mainEntityOfPage: `${process.env.SITE_URL}/manga/${manga.id}/${generateSlug(
+      title,
+    )}`,
+    headline: `${title}`,
+    description: description,
+    image: {
+      "@type": "ImageObject",
+      url: `${siteConfig.weebdex.ogURL}/og-image/manga/${manga.id}`,
+      width: 1280,
+      height: 960,
+    },
+  };
+};
