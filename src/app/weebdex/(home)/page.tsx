ce0b39be50f7c221d2c9981527868573c0027c20
iamneyk <iamneyk@gmail.com>
2026-02-28 16:11:30 +0700

    feat: temporarily disable StaffPick and LeaderBoard sections due to insufficient data

--- src/app/weebdex/(home)/page.tsx
+++ src/app/weebdex/(home)/page.tsx
@@ -89,18 +89,19 @@ export default function Page() {
           <Recently />
         </section>
 
-        <section className="mt-9 grid grid-cols-1 gap-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        {/* Temporary disabled because less of data */}
+        {/* <section className="mt-9 grid grid-cols-1 gap-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
           <div className="lg:col-span-2 xl:col-span-3 2xl:col-span-4">
-            {/* <LazyLoadComponent>
-                  <StaffPick />
-                </LazyLoadComponent> */}
+            <LazyLoadComponent>
+              <StaffPick />
+            </LazyLoadComponent>
           </div>
           <div className="lg:col-span-2">
-            {/* <LazyLoadComponent>
-                  <LeaderBoard />
-                </LazyLoadComponent> */}
+            <LazyLoadComponent>
+              <LeaderBoard />
+            </LazyLoadComponent>
           </div>
-        </section>
+        </section> */}
 
         <section className="mt-9 grid grid-cols-1 gap-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
           <div className="lg:col-span-2 xl:col-span-3 2xl:col-span-4">
