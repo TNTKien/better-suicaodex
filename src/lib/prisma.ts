c36b1a27b4c8f98c257514de3ae7ce8d015bb388
iamneyk <iamneyk@gmail.com>
2026-03-11 21:48:14 +0700

    prisma?

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,33 @@
+import "dotenv/config";
 import { PrismaClient } from "../../prisma/generated/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 const adapter = new PrismaMariaDb({
-  host: process.env.MYSQL_DATABASE_HOST,
-  user: process.env.MYSQL_DATABASE_USER,
-  password: process.env.MYSQL_DATABASE_PASSWORD,
-  database: process.env.MYSQL_DATABASE_NAME,
+  host: process.env.MYSQL_DATABASE_HOST as string,
+  user: process.env.MYSQL_DATABASE_USER as string,
+  password: process.env.MYSQL_DATABASE_PASSWORD as string,
+  database: process.env.MYSQL_DATABASE_NAME as string,
   connectionLimit: 5,
+  // allowPublicKeyRetrieval: true,
   // ssl: {
-  //   rejectUnauthorized: true,
+  //   rejectUnauthorized: false,
   // },
+  //  logger: {
+  //   network: (info) => {
+  //     console.log('PrismaAdapterNetwork', info);
+  //   },
+  //   query: (info) => {
+  //     console.log('PrismaAdapterQuery', info);
+  //   },
+  //   error: (error) => {
+  //     console.error('PrismaAdapterError', error);
+  //   },
+  //   warning: (info) => {
+  //     console.warn('PrismaAdapterWarning', info);
+  //   },
+  // }
 });
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
